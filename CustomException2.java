5e88562362c22e7118336fe3cc99d67c44ee444b
tejashwinivc <106551087+tejashwinivc@users.noreply.github.com>
2024-02-28 20:02:31 +0530

    Add files via upload

---
+++ CustomException2.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class UnderAge extends Exception {
+
+}
+
+class OverAge extends Exception
+{
+	
+}
+
+class ElectionCommission {
+	int age;
+
+	void acceptInput() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the age:");
+		age = s.nextInt();
+	}
+
+	void checkVote() throws Exception {
+		if (age >= 18 && age<=60) {
+			System.out.println("Good, Go and caste your vote");
+		}
+		else if(age < 18) {
+			UnderAge uE=new UnderAge();
+			//System.out.println("RE-ENTER");
+			throw uE;
+		}
+		else {
+			OverAge oE=new OverAge();
+			//System.out.println("You are too old to vote");
+			throw oE;
+		}
+	}
+
+}
+
+class Government {
+	void election() {
+		ElectionCommission e = new ElectionCommission();
+		try {
+			e.acceptInput();
+			e.checkVote();
+		} catch (UnderAge uE) {
+			System.out.println("You are too young to vote");
+			
+		}
+		catch (OverAge oE)
+		{
+			System.out.println("You are to old to vote");
+		}
+		catch (Exception f)
+		{
+			System.out.println("***EXCEPTION***");
+		}
+	}
+}
+
+public class CustomException2 {
+
+	public static void main(String[] args) {
+
+		Government g = new Government();
+		g.election();
+	}
+
+}
